7f651b206a62e645d60a8c9715abec87d582399c
Adriel Perkins <adrielperkins@Adriels-MacBook-Air.local>
2021-10-16 18:30:38 -0400

    feat: update adrielp rgb settings and add bongo cat animation

--- keyboards/splitkb/kyria/keymaps/adrielp/config.h
+++ keyboards/splitkb/kyria/keymaps/adrielp/config.h
@@ -16,14 +16,36 @@
 
 #pragma once
 
-#ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_ANIMATIONS
-#    define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_SWIRL
-#    define RGBLIGHT_HUE_STEP  190
-#    define RGBLIGHT_SAT_STEP  50
-#    define RGBLIGHT_VAL_STEP  50
-#    define RGBLIGHT_LIMIT_VAL 150
+#ifdef OLED_ENABLE
+  #define OLED_DISPLAY_128X64
+  #define OLED_TIMEOUT 300000
 #endif
+
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+#define NO_ACTION_ONESHOT
+#define OLED_FONT_END 127
+//#define MASTER_LEFT
+
+#ifdef RGBLIGHT_ENABLE
+    //#define RGBLIGHT_LED_MAP {0,1,2,9,8,7,4,3,5,6,19,18,17,10,11,12,15,16,14,13} // Orients Kyria LEDs to a circle around both halves.
+    //#define RBGLIGHT_LED_MAP {9,8,6,7,5,3,2,4,1,0,10,12,13,11,14,16,17,15,18,19} // Orients Kyria LEDs for a left half-right half columnar progression.
+    //#define RGBLIGHT_ANIMATIONS
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+    #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_SWIRL
+    //#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+    #define RGBLIGHT_SLEEP
+#endif
+
+//#ifdef RGBLIGHT_ENABLE
+//#    define RGBLIGHT_ANIMATIONS
+//#    define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_SWIRL
+//#    define RGBLIGHT_HUE_STEP  190
+//#    define RGBLIGHT_SAT_STEP  50
+//#    define RGBLIGHT_VAL_STEP  50
+//#    define RGBLIGHT_LIMIT_VAL 150
+//#    define RGBLIGHT_SLEEP
+//#endif
 
 // Lets you roll mod-tap keys
 #define IGNORE_MOD_TAP_INTERRUPT
@@ -31,3 +53,4 @@
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
 //#define SPLIT_USB_DETECT
 //#define NO_USB_STARTUP_CHECK
+//#define SPLIT_USB_TIMEOUT 1000
--- keyboards/splitkb/kyria/keymaps/adrielp/rules.mk
+++ keyboards/splitkb/kyria/keymaps/adrielp/rules.mk
@@ -2,3 +2,23 @@ OLED_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = no       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+RGBLIGHT_ANIMATIONS = yes
+WPM_ENABLE = yes
+
+# Disabling things for efficiency
+TAP_DANCE_ENABLE = no
+STENO_ENABLE = no
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+TERMINAL_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+SPACE_CADET_ENABLE = no
+KEY_LOCK_ENABLE = no
+AUDIO_ENABLE = no
+CONSOLE_ENABLE = no
+VELOCIKEY_ENABLE = no
+MOUSEKEY_ENABLE = no
+
+
+# Optimization
+EXTRAFLAGS += -flto
